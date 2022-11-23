8b443608b4fc5a34dda55b736dfdf4cc3f21cccc
NamekujiLSDs <okosimasu3@gmail.com>
2022-11-23 09:48:30 +0900

    z

--- js/game.js
+++ js/game.js
@@ -1,10 +1,35 @@
-const game = document.getElementById('game')
+const game = document.getElementById("game");
 
 function start() {
-    console.log("Game Loaded");
-    game.innerHTML = "";
-    const gameTitle = document.createElement('div');
-    gameTitle.textContent = "悪夢の迷宮"
-    gameTitle.setAttribute('id', 'gameTitle')
-    game.appendChild(gameTitle)
+  console.log("Game Loaded");
+  game.innerHTML = "";
+  const gameTitle = document.createElement("div");
+  gameTitle.textContent = "悪夢の迷宮";
+  gameTitle.setAttribute("id", "gameTitle");
+  game.appendChild(gameTitle);
+  const gameStart = document.createElement("input");
+  gameStart.value = "Start";
+  gameStart.setAttribute("type", "button");
+  gameStart.setAttribute("id", "startBtn");
+  gameStart.setAttribute("class", "btn startMenuBtn");
+  game.appendChild(gameStart);
+  const gameLoad = document.createElement("input");
+  gameLoad.value = "Load";
+  gameLoad.setAttribute("type", "button");
+  gameLoad.setAttribute("id", "loadBtn");
+  gameLoad.setAttribute("class", "btn startMenuBtn");
+  game.appendChild(gameLoad);
+  game.setAttribute("name", "startMenu");
+}
+
+function gSt() {
+  alert("start");
+}
+function csL() {
+  let csv = prompt("a");
+  console.log(csv);
+  let val = csv;
+  if (csv[7] === "w" || csv[7] === "a" || csv[7] === "v" || csv[7] === "m") {
+    alert("ロードされました");
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-window.onload = function () { start() };
-
-const w = ["1", "15", "10", "5", "20", "5"];
-const a = ["1", "10", "5", "10", "10", "20"];
-const m = ["1", "10", "5", "20", "12", "8"];
-const v = ["1", "11", "11", "11", "11", "11"];
+window.onload = function () {
+  start();
+};
+let cs = "";
+const w = ["1", "15", "10", "5", "20", "5", "", "w"];
+const a = ["1", "10", "5", "10", "10", "20", "", "a"];
+const m = ["1", "10", "5", "20", "12", "8", "", "m"];
+const v = ["1", "11", "11", "11", "11", "11", "", "v"];
 
 // W = Warrior
 // A = Assassin
@@ -16,3 +18,33 @@ const v = ["1", "11", "11", "11", "11", "11"];
 // [3] MP
 // [4] HP
 // [5] SPD
+// [6] Name
+// [7] Class
+
+//右クリック・文字選択の無効化
+document.onselectstart = function () {
+  return false;
+};
+document.oncontextmenu = function () {
+  return false;
+};
+
+//gameを監視してちょめちょめ
+const options = {
+  attributes: true, //属性の変化を監視
+};
+//コールバック関数
+function callback(mutationsList, observer) {
+  for (const mutation of mutationsList) {
+    if (mutation.target.getAttribute("name") === "startMenu") {
+      document.getElementById("startBtn").addEventListener("click", gSt);
+      document.getElementById("loadBtn").addEventListener("click", csL);
+    }
+  }
+}
+//ターゲット要素をDOMで取得
+const target = document.getElementById("game");
+//インスタンス化
+const obs = new MutationObserver(callback);
+//ターゲット要素の監視を開始
+obs.observe(target, options);
